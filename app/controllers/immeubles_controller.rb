a63c6fc04d99b8936b21d336627d1f85bfb75ce8
pasqal <pasq2al@gmail.com>
2011-03-20 00:07:06 +0100

    mise en place java pour ajout formulaire

--- app/controllers/immeubles_controller.rb
+++ app/controllers/immeubles_controller.rb
@@ -1,6 +1,7 @@
 class ImmeublesController < ApplicationController
   before_filter :load_dossier
   set_tab :immeuble
+
   def index
     @immeubles = @dossier.immeubles.all
   end
@@ -8,10 +9,15 @@ class ImmeublesController < ApplicationController
   def show
     @immeuble = @dossier.immeubles.find(params[:id])
     @compteur = Compteur.find_by_immeuble_id(:immeuble.id)
+
+
   end
 
   def new
     @immeuble = @dossier.immeubles.new
+    @immeuble.parcelles.build
+    @immeuble.compteurs.build
+    @immeuble.liste_pieces.build
   end
 
   def create
@@ -24,11 +30,11 @@ class ImmeublesController < ApplicationController
   end
 
   def edit
-    @immeuble = Immeuble.find(params[:id])
+    @immeuble = @dossier.immeubles.find(params[:id])
   end
 
   def update
-    @immeuble = Immeuble.find(params[:id])
+    @immeuble = @dossier.immeubles.find(params[:id])
     if @immeuble.update_attributes(params[:immeuble])
       redirect_to @immeuble, :notice  => "immeuble correctement ravalé ou modifié"
     else
@@ -37,9 +43,9 @@ class ImmeublesController < ApplicationController
   end
 
   def destroy
-    @immeuble = Immeuble.find(params[:id])
+    @immeuble = @dossier.immeubles.find(params[:id])
     @immeuble.destroy
-    redirect_to immeubles_url, :notice => "Successfully destroyed immeuble."
+    redirect_to immeubles_url, :notice => "Destruction immeuble résussie"
   end
 
 private
